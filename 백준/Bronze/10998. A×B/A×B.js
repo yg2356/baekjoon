4a1219dfdcc42415772a9ed4904611ef35ab6cf6
yg2356 <59286004+yg2356@users.noreply.github.com>
2023-09-03 12:30:57 +0900

    [Bronze V] Title: A×B, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+//let input = fs.readFileSync('input.txt').toString().split(' ');
+
+let a = parseInt(input[0]);
+let b= parseInt(input[1]);
+
+console.log(a*b);
