1991b8cafce5ec7fec736d47f82bc9a8680bdc74
yg2356 <59286004+yg2356@users.noreply.github.com>
2023-09-03 13:16:59 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+//let input = fs.readFileSync('input.txt').toString().split(' ');
+
+let a = parseInt(input[0]);
+let b= parseInt(input[1]);
+let c = parseInt(input[2]);
+
+console.log(a+b+c);
